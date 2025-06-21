5d63851c49ee2b1bfb78252f28c6b32703fd3e46
Moray Macdonald <moray.macdonald1@gmail.com>
2025-06-21 20:09:49 +0100

    Initial content

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog'
-export const SITE_DESCRIPTION = 'Welcome to my website!'
+export const SITE_TITLE = 'The Devizes Daphne Oram Festival'
+export const SITE_DESCRIPTION =
+  "Celebrating Daphne Oram's Wiltshire heritage and women in electronic music"
